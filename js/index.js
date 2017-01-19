81f28efe35c72d0af44fb5072f7927a5ef830cf8
DESKTOP-D7MLA3J\Ihab <tinawie@yahoo.com>
2017-01-19 15:57:48 +0000

    test the second div

--- js/index.js
+++ js/index.js
@@ -11,17 +11,27 @@ $(function (){
     hour=(hour*Math.PI/6)+
     (minute*Math.PI/(6*60))+
     (second*Math.PI/(360*60));
-    setPosHour(hour , radius*0.5, radius*0.07);
+    setPos(hour , radius*0.5, radius*0.07);
     //minute
     minute=(minute*Math.PI/30)+(second*Math.PI/(30*60));
-    setPosMin(minute, radius*0.8, radius*0.07);
+    setPos(minute, radius*0.8, radius*0.07);
     // second
     second=(second*Math.PI/30);
-    setPosSec(second, radius*0.9, radius*0.02);
+    setPos("second", second);
 }
 
-  function setPosSec(x, y){
-    $(".second").css("left", Math.sin((Math.PI/2)+((Math.PI/2)- i* Math.PI/6)) * $(".face").width()/2 + 0.5* $(".face").width()+"px");
+  function setPos(div, value){
+    switch (div) {
+      case "second":
+          $(".second").css("left", Math.sin(value) * $(".face").width()/2 + 0.5* $(".face").width()+"px");
+          $(".second").css("top", Math.cos(value) * $(".face").height()/2 + 0.5* $(".face").height()+"px");
+          break;
+      case "minute":
+
+          break;
+      case "hour":
+          break;
+    }
   }
   for (i = 0 ; i< 12 ; i++ ) {
     setTimeout(setLeftPos(i+1) ,1000);
